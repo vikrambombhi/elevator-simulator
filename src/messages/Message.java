856fcbb1f075ad1924e8aabe5ab7ba51214de65b
kirinrastogi <rastogikirin@gmail.com>
2019-01-24 16:59:46 -0500

    make exception handling in message parsing less idiomatic

--- src/messages/Message.java
+++ src/messages/Message.java
@@ -2,26 +2,34 @@ package messages;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public interface Message {
 	// When receiving a message, deserialize it with this, and cast it using
 	// instanceof
-	public static Message deserialize(byte[] data) throws IOException,
-			ClassNotFoundException {
+	public static Message deserialize(byte[] data) {
 		ByteArrayInputStream in = new ByteArrayInputStream(data);
-		ObjectInputStream is = new ObjectInputStream(in);
-		return (Message) is.readObject();
+		Message m = null;
+		try {
+			ObjectInputStream is = new ObjectInputStream(in);
+			m = (Message) is.readObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return m;
 	}
 
 	// This works with empty bytes postpended
 	// don't worry about message sizes or types, just cast
-	public static byte[] serialize(Message obj) throws IOException {
+	public static byte[] serialize(Message obj) {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		ObjectOutputStream os = new ObjectOutputStream(out);
-		os.writeObject(obj);
+		try {
+			ObjectOutputStream os = new ObjectOutputStream(out);
+			os.writeObject(obj);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return out.toByteArray();
 	}
 }
