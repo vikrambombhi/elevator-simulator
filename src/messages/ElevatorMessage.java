b63f1ad7b91f74ca626c8f4ca9d2adf4914903b4
kirinrastogi <rastogikirin@gmail.com>
2019-01-24 14:40:08 -0500

    add ser and deser of messages

---
+++ src/messages/ElevatorMessage.java
@@ -0,0 +1,12 @@
+package messages;
+
+import java.io.Serializable;
+
+public class ElevatorMessage implements Message, Serializable {
+
+	private static final long serialVersionUID = -2950505368297636750L;
+
+	public ElevatorMessage() {
+
+	}
+}
--- src/messages/Message.java
+++ src/messages/Message.java
@@ -1,11 +1,27 @@
 package messages;
 
-// This is the class that the messages inherit from
-// it has a toObject method that you use after switching on
-// the byte array from a packet
-public abstract class Message {
-	// Take a byte array and return a type of message
-	// The caller will switch on the first bytes of an array
-	// then call this and cast it to whatever subclass they want.
-	public abstract Message toObject(byte[] message);
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public interface Message {
+	// When receiving a message, deserialize it with this, and cast it using
+	// instanceof
+	public static Message deserialize(byte[] data) throws IOException,
+			ClassNotFoundException {
+		ByteArrayInputStream in = new ByteArrayInputStream(data);
+		ObjectInputStream is = new ObjectInputStream(in);
+		return (Message) is.readObject();
+	}
+
+	// This works with empty bytes postpended
+	// don't worry about message sizes or types, just cast
+	public static byte[] serialize(Message obj) throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		ObjectOutputStream os = new ObjectOutputStream(out);
+		os.writeObject(obj);
+		return out.toByteArray();
+	}
 }
