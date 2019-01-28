05a2fd6fede61fc8148678d23336def0aca99ca9
vikrambombhi <vbombhi@gmail.com>
2019-01-28 14:00:44 -0500

    creates message types for elevator subsystem

--- src/messages/ElevatorMessage.java
+++ src/messages/ElevatorMessage.java
@@ -4,9 +4,43 @@ import java.io.Serializable;
 
 public class ElevatorMessage implements Message, Serializable {
 
-	private static final long serialVersionUID = -2950505368297636750L;
+    private static final long serialVersionUID = -2950505368297636750L;
 
-	public ElevatorMessage() {
+    enum MessageType {
+        STOP, GOUP, GODOWN, OPENDOOR, CLOSEDOOR;
+    }
 
-	}
+    MessageType messageType;
+
+    /*
+     * Constructor for empty elevator message
+     */
+    public ElevatorMessage() {}
+
+    /*
+     * Constructor for elevator message
+     *
+     * @param messageType   The type of message to create
+     */
+    public ElevatorMessage(MessageType messageType) {
+        this.messageType = messageType;
+    }
+
+    /*
+     * Returns the type of message
+     *
+     * @return the type of message
+     */
+    public MessageType getMessageType() {
+        return this.messageType;
+    }
+
+    /*
+     * Set message type
+     *
+     * @param messageType   Message type to set
+     */
+    public void setMessageType(MessageType messageType) {
+        this.messageType = messageType;
+    }
 }
