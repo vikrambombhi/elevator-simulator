892f156e3bfdc9c6bd4744944ef44d70850ff6ab
kirinrastogi <rastogikirin@gmail.com>
2019-01-29 12:19:59 -0500

    add floor to arrivalmessage

--- src/messages/ArrivalMessage.java
+++ src/messages/ArrivalMessage.java
@@ -10,8 +10,25 @@ public class ArrivalMessage implements Message, Serializable {
 	 */
 	private static final long serialVersionUID = 7521529484689926656L;
 
+	private int floor;
+
 	public ArrivalMessage() {
 
+	}
+
+	/**
+	 * @return the floor
+	 */
+	public int getFloor() {
+		return floor;
+	}
+
+	/**
+	 * @param floor
+	 *            the floor to set
+	 */
+	public void setFloor(int floor) {
+		this.floor = floor;
 	}
 
 }
