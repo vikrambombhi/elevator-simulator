440236b4cca7d7bc6d88bab63ca5512d9516c583
kirinrastogi <rastogikirin@gmail.com>
2019-01-29 11:44:37 -0500

    update readme and message classes

--- src/messages/ElevatorMessage.java
+++ src/messages/ElevatorMessage.java
@@ -2,45 +2,47 @@ package messages;
 
 import java.io.Serializable;
 
+// Any subsystem sending messages to an elevator should use this message class
 public class ElevatorMessage implements Message, Serializable {
 
-    private static final long serialVersionUID = -2950505368297636750L;
+	private static final long serialVersionUID = -2950505368297636750L;
 
-    public enum MessageType {
-        STOP, GOUP, GODOWN, OPENDOOR, CLOSEDOOR;
-    }
+	public enum MessageType {
+		STOP, GOUP, GODOWN, OPENDOOR, CLOSEDOOR;
+	}
 
-    MessageType messageType;
+	MessageType messageType;
 
-    /*
-     * Constructor for empty elevator message
-     */
-    public ElevatorMessage() {}
+	/*
+	 * Constructor for empty elevator message
+	 */
+	public ElevatorMessage() {
+	}
 
-    /*
-     * Constructor for elevator message
-     *
-     * @param messageType   The type of message to create
-     */
-    public ElevatorMessage(MessageType messageType) {
-        this.messageType = messageType;
-    }
+	/*
+	 * Constructor for elevator message
+	 * 
+	 * @param messageType The type of message to create
+	 */
+	public ElevatorMessage(MessageType messageType) {
+		this.messageType = messageType;
+	}
 
-    /*
-     * Returns the type of message
-     *
-     * @return the type of message
-     */
-    public MessageType getMessageType() {
-        return this.messageType;
-    }
+	/*
+	 * Returns the type of message
+	 * 
+	 * @return the type of message
+	 */
+	public MessageType getMessageType() {
+		return this.messageType;
+	}
 
-    /*
-     * Set message type
-     *
-     * @param messageType   Message type to set
-     */
-    public void setMessageType(MessageType messageType) {
-        this.messageType = messageType;
-    }
+	/*
+	 * Set message type
+	 * 
+	 * @param messageType Message type to set
+	 */
+	public void setMessageType(MessageType messageType) {
+		this.messageType = messageType;
+	}
 }
