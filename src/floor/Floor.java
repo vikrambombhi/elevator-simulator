b8a8421f93d3180233b5cdd9dae2e0726aab4df7
vikrambombhi <vbombhi@gmail.com>
2019-01-30 11:19:01 -0500

    fix spacing. >:( my vimrc needs work

--- src/floor/Floor.java
+++ src/floor/Floor.java
@@ -3,8 +3,8 @@ package floor;
 import java.util.*;
 
 public class Floor {
-	public static void main(String args[]) {
+    public static void main(String args[]) {
         File file = new File();
         ArrayList<messages.ElevatorRequestMessage> messages = file.readFile("input.txt");
-	}
+    }
 }
