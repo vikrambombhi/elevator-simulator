29ad3a81a091715c46d05b2a6f212f9259d90b34
kirinrastogi <rastogikirin@gmail.com>
2019-02-02 18:00:35 -0500

    use floorarrivalmessage instead of arrivalmessage

--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -7,16 +7,15 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.LinkedList;
 
-import messages.ArrivalMessage;
+import elevator.Elevator;
 import messages.ElevatorMessage;
 import messages.ElevatorMessage.MessageType;
-import messages.ElevatorRequestMessage;
+import messages.FloorArrivalMessage;
 import messages.FloorRequestMessage;
 import messages.Message;
-import elevator.Elevator;
 
 public class Scheduler {
-	private DatagramSocket sock;
+	private DatagramSocket recvSock, sendSock;
 	private LinkedList<Integer> queue;
 
 	public Scheduler() {
@@ -24,7 +23,8 @@ public class Scheduler {
 			// Construct a datagram socket and bind it to port 3000
 			// on the local host machine. This socket will be used to
 			// receive UDP Datagram packets.
-			sock = new DatagramSocket(3000);
+			recvSock = new DatagramSocket(3000);
+			sendSock = new DatagramSocket();
 
 			// to test socket timeout (2 seconds)
 			// receiveSocket.setSoTimeout(2000);
@@ -38,31 +38,49 @@ public class Scheduler {
 	public void schedule() {
 		// continuously listen for packets from elevators or floors
 		// handle them
-		Message m = Message.deserialize(Message.receive(sock).getData());
-		if (m instanceof ElevatorRequestMessage) {
-			// add request to elevator floor queue
-			ElevatorRequestMessage erm = (ElevatorRequestMessage) m;
-			queue.add(erm.getOriginFloor());
-		} else if (m instanceof FloorRequestMessage) {
+		System.out.println("Scheduler: Waiting for message");
+		Message m = Message.deserialize(Message.receive(recvSock).getData());
+		if (m instanceof FloorRequestMessage) {
 			// add request to pick up elevators
 			FloorRequestMessage frm = (FloorRequestMessage) m;
 			queue.add(frm.getFloor());
-		} else if (m instanceof ArrivalMessage) {
+			System.out.println("New queue: " + queue.toString());
+		} else if (m instanceof FloorArrivalMessage) {
 			// this means that an elevator arrived at a floor, tell it what to
 			// do next. Either open doors, or go up or down
-			ArrivalMessage am = (ArrivalMessage) m;
+			// this also means that the elevator's doors are now closing, and there
+			// could be floors to enqueue
+			FloorArrivalMessage am = (FloorArrivalMessage) m;
 			// tell the elevator where to go based on the queue
+			for (int floor : am.getDestinations()) {
+				queue.add(floor);
+				System.out.println("New queue: " + queue.toString());
+			}
 
-			// if am floor is queue.peek(), dequeue
+			// if am floor is queue.peek(), dequeue and tell the elevator
+			// to stop and open doors
+			if (am.getFloor() == queue.peek()) {
+				queue.remove();
+				sendToElevator(MessageType.STOP);
+				return;
+			}
 
 			// create a new ElevatorMessage, and send it to the elevator
 			// based on what floor it should go to next
 			// sendToElevator(direction);
+			if (am.getFloor() - queue.peek() > 0) {
+				// go down
+				sendToElevator(MessageType.GODOWN);
+			} else {
+				// go up
+				sendToElevator(MessageType.GOUP);
+			}
 		}
 
 	}
 
 	private void sendToElevator(MessageType action) {
+		System.out.println("Sending message of type " + action);
 		byte[] data = Message.serialize((new ElevatorMessage(action)));
 		InetAddress destHost = null;
 		try {
@@ -72,26 +90,29 @@ public class Scheduler {
 			System.exit(1);
 		}
 		DatagramPacket pack = new DatagramPacket(data, data.length, destHost, Elevator.PORT);
-		Message.send(sock, pack);
+		Message.send(sendSock, pack);
 	}
 
 	public void close() {
-		sock.close();
+		recvSock.close();
+		sendSock.close();
 	}
 
 	public void run() {
 		try {
 			while (true) {
+				sendToElevator(MessageType.GOUP);
 				schedule();
 			}
 		} catch (Exception e) {
+			e.printStackTrace();
 			close();
 		}
 	}
 
 	public static void main(String args[]) {
 		System.out.println("Scheduler: Starting on port 3000");
-		Scheduler proxy = new Scheduler();
-		proxy.run();
+		Scheduler sched = new Scheduler();
+		sched.run();
 	}
 }
