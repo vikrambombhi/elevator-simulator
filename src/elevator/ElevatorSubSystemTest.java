903b8bcbb9b2657bdfe516360a01422c30368381
Jacky Chiu <jackyzhanchiu@gmail.com>
2019-02-11 15:16:52 -0500

    test independent floor updates

--- src/elevator/ElevatorSubSystemTest.java
+++ src/elevator/ElevatorSubSystemTest.java
@@ -6,9 +6,13 @@ import junit.framework.TestCase;
 
 public class ElevatorSubSystemTest extends TestCase {
 
+    public void testElevatorSubSystemElevatorsCreated() {
+        ElevatorSubSystem subsystem = new ElevatorSubSystem(9);
+        assertEquals(9, subsystem.getElevators().length);
+    }
+
     public void testElevatorMessageStateMachine() {
         ElevatorSubSystem subsystem = new ElevatorSubSystem(1);
-        assertEquals(1, subsystem.getElevators().length);
 
         // Start off stopped and closed
         Elevator elevator = subsystem.getElevators()[0];
@@ -26,5 +30,22 @@ public class ElevatorSubSystemTest extends TestCase {
         // Test GODOWN
         subsystem.handleMessage(new ElevatorMessage(0, messages.ElevatorMessage.MessageType.GODOWN));
         assertEquals(Elevator.State.MOVING_DOWN, elevator.getState());
+    }
+
+    public void testFloorArrivalMessage() {
+        ElevatorSubSystem subsystem = new ElevatorSubSystem(3);
+        Elevator[] elevators = subsystem.getElevators();
+
+        FloorArrivalMessage fm = new FloorArrivalMessage();
+        fm.setDirection(messages.ElevatorMessage.MessageType.GOUP);
+        fm.setFloor(9);
+        fm.setElevator(2);
+
+        subsystem.handleMessage(fm);
+
+        // assert that floor arrival only effects the target elevator
+        assertEquals(0, elevators[0].getFloor());
+        assertEquals(0, elevators[1].getFloor());
+        assertEquals(9, elevators[2].getFloor());
     }
 }
