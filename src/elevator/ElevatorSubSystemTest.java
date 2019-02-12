5c329033adfac85f32822e59a5f7b00454d5b52e
Jacky Chiu <jackyzhanchiu@gmail.com>
2019-02-11 21:37:12 -0500

    test independent states in elevators

--- src/elevator/ElevatorSubSystemTest.java
+++ src/elevator/ElevatorSubSystemTest.java
@@ -32,12 +32,34 @@ public class ElevatorSubSystemTest extends TestCase {
         assertEquals(Elevator.State.MOVING_DOWN, elevator.getState());
     }
 
+    public void testElevatorMessageIsIndependent() {
+        ElevatorSubSystem subsystem = new ElevatorSubSystem(5);
+        Elevator[] elevators = subsystem.getElevators();
+
+        // Test only elevator 3 changes state
+        subsystem.handleMessage(new ElevatorMessage(3, messages.ElevatorMessage.MessageType.GOUP));
+        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevators[0].getState());
+        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevators[1].getState());
+        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevators[2].getState());
+        assertEquals(Elevator.State.MOVING_UP, elevators[3].getState());
+        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevators[4].getState());
+
+        // Test only elevator 1 changes state
+        elevators[1].setFloor(1);
+        subsystem.handleMessage(new ElevatorMessage(1, messages.ElevatorMessage.MessageType.GODOWN));
+        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevators[0].getState());
+        assertEquals(Elevator.State.MOVING_DOWN, elevators[1].getState());
+        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevators[2].getState());
+        assertEquals(Elevator.State.MOVING_UP, elevators[3].getState());
+        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevators[4].getState());
+    }
+
     public void testFloorArrivalMessage() {
         ElevatorSubSystem subsystem = new ElevatorSubSystem(3);
         Elevator[] elevators = subsystem.getElevators();
 
         FloorArrivalMessage fm = new FloorArrivalMessage();
-        fm.setDirection(messages.ElevatorMessage.MessageType.GOUP);
+        fm.setDirection(messages.ElevatorRequestMessage.Direction.UP);
         fm.setFloor(9);
         fm.setElevator(2);
 
