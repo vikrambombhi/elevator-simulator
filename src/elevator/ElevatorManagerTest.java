f4d37a26d44be64aeee09b39da1ac5d2a188d903
vikrambombhi <vbombhi@gmail.com>
2019-02-13 17:48:02 -0500

    fix spacing to tabs, sorry jacky

--- src/elevator/ElevatorManagerTest.java
+++ src/elevator/ElevatorManagerTest.java
@@ -14,132 +14,132 @@ import junit.framework.TestCase;
 
 public class ElevatorManagerTest extends TestCase {
 
-    private DatagramSocket sendSocket;
+	private DatagramSocket sendSocket;
 
-    @Before
-    public void setUp() {
-        try {
-            sendSocket = new DatagramSocket();
-        } catch (SocketException se) {
-            se.printStackTrace();
-            System.exit(1);
-        }
-    }
+	@Before
+	public void setUp() {
+		try {
+			sendSocket = new DatagramSocket();
+		} catch (SocketException se) {
+			se.printStackTrace();
+			System.exit(1);
+		}
+	}
 
-    @Test
-    public void testElevatorCreation() {
-        ElevatorManager manager = new ElevatorManager(9);
-        assertEquals(9, manager.getElevatorSubsystems().length);
-        manager.close();
-    }
+	@Test
+	public void testElevatorCreation() {
+		ElevatorManager manager = new ElevatorManager(9);
+		assertEquals(9, manager.getElevatorSubsystems().length);
+		manager.close();
+	}
 
-    @Test
-    public void testElevatorMessageStateMachine() {
-        ElevatorManager manager = new ElevatorManager(1);
-        ElevatorSubSystem subsystem = manager.getElevatorSubsystems()[0];
+	@Test
+	public void testElevatorMessageStateMachine() {
+		ElevatorManager manager = new ElevatorManager(1);
+		ElevatorSubSystem subsystem = manager.getElevatorSubsystems()[0];
 
-        // Start off stopped and closed
-        Elevator elevator = subsystem.getElevator();
-        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, subsystem.getElevator().getState());
+		// Start off stopped and closed
+		Elevator elevator = subsystem.getElevator();
+		assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, subsystem.getElevator().getState());
 
-        // Test GOUP
-        subsystem.handleMessage(new ElevatorMessage(messages.ElevatorMessage.MessageType.GOUP));
-        assertEquals(Elevator.State.MOVING_UP, subsystem.getElevator().getState());
+		// Test GOUP
+		subsystem.handleMessage(new ElevatorMessage(messages.ElevatorMessage.MessageType.GOUP));
+		assertEquals(Elevator.State.MOVING_UP, subsystem.getElevator().getState());
 
-        // Test STOP
-        subsystem.handleMessage(new ElevatorMessage(messages.ElevatorMessage.MessageType.STOP));
-        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, subsystem.getElevator().getState());
-        subsystem.getElevator().setFloor(1);
+		// Test STOP
+		subsystem.handleMessage(new ElevatorMessage(messages.ElevatorMessage.MessageType.STOP));
+		assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, subsystem.getElevator().getState());
+		subsystem.getElevator().setFloor(1);
 
-        // Test GODOWN
-        subsystem.handleMessage(new ElevatorMessage(messages.ElevatorMessage.MessageType.GODOWN));
-        assertEquals(Elevator.State.MOVING_DOWN, subsystem.getElevator().getState());
+		// Test GODOWN
+		subsystem.handleMessage(new ElevatorMessage(messages.ElevatorMessage.MessageType.GODOWN));
+		assertEquals(Elevator.State.MOVING_DOWN, subsystem.getElevator().getState());
 
-        manager.close();
-    }
+		manager.close();
+	}
 
-    @Test
-    public void testElevatorMessageIsIndependent() {
-        ElevatorManager manager = new ElevatorManager(5);
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                manager.run();
-            }
-        });
-        t.start();
+	@Test
+	public void testElevatorMessageIsIndependent() {
+		ElevatorManager manager = new ElevatorManager(5);
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				manager.run();
+			}
+		});
+		t.start();
 
-        ElevatorSubSystem[] elevatorSubSystems = manager.getElevatorSubsystems();
+		ElevatorSubSystem[] elevatorSubSystems = manager.getElevatorSubsystems();
 
-        // Test only elevator 3 changes state
-        byte[] data = Message.serialize(new ElevatorMessage(messages.ElevatorMessage.MessageType.GOUP));
-        DatagramPacket packet = new DatagramPacket(data, data.length,
-                SimulationVars.elevatorAddresses[3], SimulationVars.elevatorPorts[3]);
-        Message.send(sendSocket, packet);
+		// Test only elevator 3 changes state
+		byte[] data = Message.serialize(new ElevatorMessage(messages.ElevatorMessage.MessageType.GOUP));
+		DatagramPacket packet = new DatagramPacket(data, data.length,
+				SimulationVars.elevatorAddresses[3], SimulationVars.elevatorPorts[3]);
+		Message.send(sendSocket, packet);
 
-        try { Thread.sleep(500); } catch (InterruptedException e) { }
+		try { Thread.sleep(500); } catch (InterruptedException e) { }
 
-        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevatorSubSystems[0].getElevator().getState());
-        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevatorSubSystems[1].getElevator().getState());
-        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevatorSubSystems[2].getElevator().getState());
-        assertEquals(Elevator.State.MOVING_UP, elevatorSubSystems[3].getElevator().getState());
-        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevatorSubSystems[4].getElevator().getState());
+		assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevatorSubSystems[0].getElevator().getState());
+		assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevatorSubSystems[1].getElevator().getState());
+		assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevatorSubSystems[2].getElevator().getState());
+		assertEquals(Elevator.State.MOVING_UP, elevatorSubSystems[3].getElevator().getState());
+		assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevatorSubSystems[4].getElevator().getState());
 
-        // Test only elevator 1 changes state
-        elevatorSubSystems[1].getElevator().setFloor(1);
-        data = Message.serialize(new ElevatorMessage(messages.ElevatorMessage.MessageType.GODOWN));
-        packet = new DatagramPacket(data, data.length,
-                SimulationVars.elevatorAddresses[1], SimulationVars.elevatorPorts[1]);
-        Message.send(sendSocket, packet);
+		// Test only elevator 1 changes state
+		elevatorSubSystems[1].getElevator().setFloor(1);
+		data = Message.serialize(new ElevatorMessage(messages.ElevatorMessage.MessageType.GODOWN));
+		packet = new DatagramPacket(data, data.length,
+				SimulationVars.elevatorAddresses[1], SimulationVars.elevatorPorts[1]);
+		Message.send(sendSocket, packet);
 
-        try { Thread.sleep(500); } catch (InterruptedException e) { }
+		try { Thread.sleep(500); } catch (InterruptedException e) { }
 
-        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevatorSubSystems[0].getElevator().getState());
-        assertEquals(Elevator.State.MOVING_DOWN, elevatorSubSystems[1].getElevator().getState());
-        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevatorSubSystems[2].getElevator().getState());
-        assertEquals(Elevator.State.MOVING_UP, elevatorSubSystems[3].getElevator().getState());
-        assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevatorSubSystems[4].getElevator().getState());
+		assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevatorSubSystems[0].getElevator().getState());
+		assertEquals(Elevator.State.MOVING_DOWN, elevatorSubSystems[1].getElevator().getState());
+		assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevatorSubSystems[2].getElevator().getState());
+		assertEquals(Elevator.State.MOVING_UP, elevatorSubSystems[3].getElevator().getState());
+		assertEquals(Elevator.State.STOPPED_DOORS_CLOSED, elevatorSubSystems[4].getElevator().getState());
 
-        t.interrupt();
-        manager.close();
-    }
+		t.interrupt();
+		manager.close();
+	}
 
-    @Test
-    public void testFloorArrivalMessage() {
-        ElevatorManager manager = new ElevatorManager(3);
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                manager.run();
-            }
-        });
-        t.start();
+	@Test
+	public void testFloorArrivalMessage() {
+		ElevatorManager manager = new ElevatorManager(3);
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				manager.run();
+			}
+		});
+		t.start();
 
-        ElevatorSubSystem[] elevatorSubSystems = manager.getElevatorSubsystems();
+		ElevatorSubSystem[] elevatorSubSystems = manager.getElevatorSubsystems();
 
-        FloorArrivalMessage fm = new FloorArrivalMessage();
-        fm.setDirection(messages.ElevatorRequestMessage.Direction.UP);
-        fm.setFloor(9);
-        fm.setElevator(2);
+		FloorArrivalMessage fm = new FloorArrivalMessage();
+		fm.setDirection(messages.ElevatorRequestMessage.Direction.UP);
+		fm.setFloor(9);
+		fm.setElevator(2);
 
-        byte[] data = Message.serialize(fm);
-        DatagramPacket packet = new DatagramPacket(data, data.length,
-                SimulationVars.elevatorAddresses[1], SimulationVars.elevatorPorts[1]);
-        Message.send(sendSocket, packet);
+		byte[] data = Message.serialize(fm);
+		DatagramPacket packet = new DatagramPacket(data, data.length,
+				SimulationVars.elevatorAddresses[1], SimulationVars.elevatorPorts[1]);
+		Message.send(sendSocket, packet);
 
-        try { Thread.sleep(500); } catch (InterruptedException e) { }
+		try { Thread.sleep(500); } catch (InterruptedException e) { }
 
-        // assert that floor arrival only effects the target elevator
-        assertEquals(0, elevatorSubSystems[0].getElevator().getFloor());
-        assertEquals(0, elevatorSubSystems[1].getElevator().getFloor());
-        assertEquals(9, elevatorSubSystems[2].getElevator().getFloor());
+		// assert that floor arrival only effects the target elevator
+		assertEquals(0, elevatorSubSystems[0].getElevator().getFloor());
+		assertEquals(0, elevatorSubSystems[1].getElevator().getFloor());
+		assertEquals(9, elevatorSubSystems[2].getElevator().getFloor());
 
-        t.interrupt();
-        manager.close();
-    }
+		t.interrupt();
+		manager.close();
+	}
 
-    @After
-    public void tearDown() {
-        sendSocket.close();
-    }
+	@After
+	public void tearDown() {
+		sendSocket.close();
+	}
 }
