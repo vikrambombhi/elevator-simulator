4bae4ae24e267e6ba0aa7909e5c3ac0f7de6b62c
connorpoland <35580577+connorpoland@users.noreply.github.com>
2019-02-16 11:41:07 -0500

    utilizes an imput file for requests
    removed hard coded requests
    input.txt now produces the previous behaviour
    SimulationVars updated to store file name and a time scalar so simulations can be done at a faster rate

--- src/floor/RequestSimulator.java
+++ src/floor/RequestSimulator.java
@@ -1,5 +1,7 @@
 package floor;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -14,6 +16,7 @@ public class RequestSimulator implements Runnable{
 
 	private DatagramSocket sendSocket;
 	private int floorNum;
+	private int time;
 
 	public RequestSimulator(int f) {
 		try {
@@ -24,35 +27,73 @@ public class RequestSimulator implements Runnable{
 		}
 
 		floorNum = f;
+		time = 0;
 	}
 
 	@Override
 	public void run() {
+		ElevatorRequestMessage m;
+		FloorMetaMessage f;
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(SimulationVars.inputFile))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] parts = line.split(" ");
 
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
+				//our next message
+				if (Integer.parseInt(parts[1]) == floorNum) {
+					//make the messages
+					m = new ElevatorRequestMessage();
+					if (parts[2].equals("Up")) {
+						m.setDirection(Direction.UP);
+					} else {
+						m.setDirection(Direction.DOWN);
+					}
+					m.setOriginFloor(floorNum);
+					byte[] schedulerData = Message.serialize(m);
+					DatagramPacket schedulerPacket = new DatagramPacket(schedulerData, schedulerData.length, SimulationVars.schedulerAddress, SimulationVars.schedulerPort);
+					
+					f = new FloorMetaMessage(true);
+					f.setStartingFloor(floorNum);
+					f.setDestinationFloor(Integer.parseInt(parts[3]));
+					byte[] metaData = Message.serialize(f);
+					DatagramPacket metaPacket = new DatagramPacket(metaData, metaData.length, SimulationVars.floorAddresses[floorNum], SimulationVars.floorPorts[floorNum]);
+					
+					//sleep until its time to send
+					try {
+						Thread.sleep(getSleepTime(parts[0]));
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					
+					//send the messages
+					System.out.println("hello");
+					Message.send(sendSocket, schedulerPacket);
+					Message.send(sendSocket, metaPacket);
+				}
+			}
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		//for the scheduler
-		ElevatorRequestMessage m = new ElevatorRequestMessage();
-		m.setDirection(Direction.UP);
-		m.setOriginFloor(floorNum);
-		byte[] data = Message.serialize(m);
-		DatagramPacket sendPacket = new DatagramPacket(data, data.length, SimulationVars.schedulerAddress, SimulationVars.schedulerPort);
-		Message.send(sendSocket, sendPacket);
-
-		//for the origin floor
-		FloorMetaMessage f = new FloorMetaMessage(true);
-		if (floorNum == SimulationVars.numberOfFloors-1) {
-			f.setDestinationFloor(0);
-		} else {
-			f.setDestinationFloor(SimulationVars.numberOfFloors-1);
-		}
-		f.setStartingFloor(floorNum);
-		data = Message.serialize(f);
-		sendPacket = new DatagramPacket(data, data.length, SimulationVars.floorAddresses[floorNum], SimulationVars.floorPorts[floorNum]);
-		Message.send(sendSocket, sendPacket);
+	}
+	
+	public int getSleepTime(String t) {
+		String[] parts = t.split(":");
+		int sleepTime = 0;
+		//add milliseconds
+		sleepTime += Integer.parseInt(parts[2].substring(3));
+		//add seconds
+		sleepTime += (Integer.parseInt(parts[2].substring(0, 2))*1000);
+		//add mins 
+		sleepTime += (Integer.parseInt(parts[1])*60000);
+		//add hours
+		sleepTime += (Integer.parseInt(parts[0])*3600000);
+		
+		//time until we reach the time of request
+		sleepTime = sleepTime - time;
+		//when the thread wakes up from this message it will be.. time + time slept
+		time = sleepTime + time;
+		
+		return sleepTime/SimulationVars.timeScalar;
 	}
 }
